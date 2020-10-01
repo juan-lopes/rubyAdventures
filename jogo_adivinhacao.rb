2c39e3d1f373d0f4a2e881e643b805f4ed51e060
Juan <65719935+juan-lopes@users.noreply.github.com>
2020-09-30 22:26:06 -0300

    Add files via upload
    Criação de novas funções, pequenos ajustes nas existentes e solução de um crash do jogo

--- jogo_adivinhacao.rb
+++ jogo_adivinhacao.rb
@@ -6,31 +6,45 @@ def boas_vindas
 end
 
 def sorteio_de_numero
+    puts "Escolhendo um número secreto entre 0 e 200..."
     numero_secreto = 10
+    puts "Tente adivinhas o numero!\n"
+    return numero_secreto
 end
 
-boas_vindas
-sorteio_de_numero
-limite_de_tentativas = 5
-
-for tentativa in 1..limite_de_tentativas
-    puts "Tentativa" + tentativa.to_s + " de " + limite_de_tentativas.to_s
+def pede_um_numero(tentativa, limite_de_tentativas)
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
     puts "Entre um numero"
     chute = gets
     puts "\nSerá que acertou o chute? Você chutou " + chute
+    return chute.to_i
+end
 
+def verifica_se_acertou(numero_secreto, chute)
     acertou = numero_secreto == chute.to_i
 
     if acertou
         puts "Acertou"
-        break
+        return true
     else 
-        acertou = numero_secreto > chute.to_i
+        maior = numero_secreto > chute.to_i
         if maior   
             puts "O numero secreto é maior"
         else
             puts "O numero secreto é menor"
         end
+    end
+    return false
+end
 
+boas_vindas
+numero_secreto = sorteio_de_numero
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+    chute = pede_um_numero(tentativa, limite_de_tentativas)
+    if verifica_se_acertou(numero_secreto, chute)
+        break
     end
 end
